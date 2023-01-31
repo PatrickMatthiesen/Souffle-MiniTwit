52a6aa1c2d94de77eb0e4a333604e29b3718e342
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-01-31 17:44:49 +0100

    Update minitwit.py

--- minitwit.py
+++ minitwit.py
@@ -17,11 +17,11 @@ from datetime import datetime
 from contextlib import closing
 from flask import Flask, request, session, url_for, redirect, \
     render_template, abort, g, flash
-from werkzeug import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # configuration
-DATABASE = '/tmp/minitwit.db'
+DATABASE = 'minitwit.db'
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
