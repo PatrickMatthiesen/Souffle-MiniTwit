cfe67eb37a30371b8ddd18febeadbe9f6296041f
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-04-25 12:00:41 +0200

    add nginx conf

--- MiniTwit/InitSwarm.sh
+++ MiniTwit/InitSwarm.sh
@@ -3,14 +3,21 @@
 managerIp="<managerIp>"
 workerIp1="<workerIp1>"
 workerIp2="<workerIp2>"
+loadBalancerIp="<loadBalancerIp>"
 DOCKER_USERNAME="<DOCKER_USERNAME>"
+CONNECTION_STRING="<CONNECTION_STRING>"
+SERILOG_URI="<SERILOG_URI>"
+
 
 managerToken=$(ssh """$managerIp""" "docker swarm init --advertise-addr ""$managerIp""")
+ssh """$loadBalancerIp""" "docker swarm init --advertise-addr ""$loadBalancerIp"""
 
 ssh """$workerIp1""" "docker swarm join --token ""$managerToken"""
 ssh """$workerIp2""" "docker swarm join --token ""$managerToken"""
 
-# overlay network on manager node with ssh:
+# overlay network on manager node:
 ssh """$managerIp""" "docker network create --driver overlay minitwit-net"
 
-ssh """$managerIp""" "docker service create --name minitwit-souffle --publish 80 --replicas 2 ""$DOCKER_USERNAME""/minitwit-souffle:latest"
+ssh """$managerIp""" "docker service create --name minitwit-souffle --publish 8080:80 --replicas 2 -e ""$CONNECTION_STRING"" -e ""$SERILOG_URI"" ""$DOCKER_USERNAME""/minitwit-souffle:latest"
+
+ssh """$loadBalancerIp""" "docker service create --name minitwit-nginx --publish 80:80 --replicas 1 --network minitwit-net""$DOCKER_USERNAME""/minitwit-nginx:latest"
