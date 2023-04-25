41e91c3567dd01b6817ab2bcc51ebb046816b7c4
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-04-25 12:40:27 +0200

    Update InitSwarm.sh

--- MiniTwit/InitSwarm.sh
+++ MiniTwit/InitSwarm.sh
@@ -11,6 +11,7 @@ SERILOG_URI="<SERILOG_URI>"
 
 managerToken=$(ssh """$managerIp""" "docker swarm init --advertise-addr ""$managerIp""")
 ssh """$loadBalancerIp""" "docker swarm init --advertise-addr ""$loadBalancerIp"""
+scp ".\nginx.conf" root@$loadBalancerIp:default.conf
 
 ssh """$workerIp1""" "docker swarm join --token ""$managerToken"""
 ssh """$workerIp2""" "docker swarm join --token ""$managerToken"""
@@ -20,4 +21,4 @@ ssh """$managerIp""" "docker network create --driver overlay minitwit-net"
 
 ssh """$managerIp""" "docker service create --name minitwit-souffle --publish 8080:80 --replicas 2 -e ""$CONNECTION_STRING"" -e ""$SERILOG_URI"" ""$DOCKER_USERNAME""/minitwit-souffle:latest"
 
-ssh """$loadBalancerIp""" "docker service create --name minitwit-nginx --publish 80:80 --replicas 1 --network minitwit-net""$DOCKER_USERNAME""/minitwit-nginx:latest"
+ssh """$loadBalancerIp""" "docker service create --name minitwit-nginx --publish 80:80 --replicas 1 -v /root/default.conf:/etc/nginx/conf.d/default.conf nginx"
