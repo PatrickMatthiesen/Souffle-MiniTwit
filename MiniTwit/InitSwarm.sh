4b31c940a2189266164b3219e60c0765bac975b5
LysetsDal <asly@itu.dk>
2023-05-02 12:56:58 +0200

    Testing NGINX with swarm on droplets

--- MiniTwit/InitSwarm.sh
+++ MiniTwit/InitSwarm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-managerIp="<managerIp>"
-workerIp1="<workerIp1>"
-workerIp2="<workerIp2>"
-loadBalancerIp="<loadBalancerIp>"
-DOCKER_USERNAME="<DOCKER_USERNAME>"
-CONNECTION_STRING="<CONNECTION_STRING>"
-SERILOG_URI="<SERILOG_URI>"
+managerIp="134.209.254.143"
+workerIp1="165.227.161.26"
+# workerIp2="<workerIp2>"
+loadBalancerIp="68.183.213.41"
+DOCKER_USERNAME="subjective0"
+CONNECTION_STRING='"ConnectionStrings:MiniTwit-db"="Server=174.138.101.165;Database=MiniTwit-db;User Id=sa;Password=a282f131-5323-419c-9c8e-55535d486550;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True"'
+SERILOG_URI="http://174.138.101.165:9090/"
 
 Green='\033[0;32m'
 NC='\033[0m' # No Color
