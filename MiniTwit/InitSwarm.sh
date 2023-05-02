a7ef63fd4e24dfb758e280461b12f24658448dd3
LysetsDal <asly@itu.dk>
2023-05-02 16:07:45 +0200

    "Proxy-Forward-X"

--- MiniTwit/InitSwarm.sh
+++ MiniTwit/InitSwarm.sh
@@ -3,7 +3,7 @@
 managerIp="134.209.254.143"
 workerIp1="165.227.161.26"
 # workerIp2="<workerIp2>"
-loadBalancerIp="68.183.213.41"
+loadBalancerIp="164.92.200.109"
 DOCKER_USERNAME="subjective0"
 CONNECTION_STRING='"ConnectionStrings:MiniTwit-db"="Server=174.138.101.165;Database=MiniTwit-db;User Id=sa;Password=a282f131-5323-419c-9c8e-55535d486550;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True"'
 SERILOG_URI="http://174.138.101.165:9090/"
@@ -15,9 +15,6 @@ echo -e "${Green}Init swarm on managers${NC}"
 managerToken=$(ssh """root@$managerIp""" "docker swarm init --advertise-addr ""$managerIp""")
 ssh """root@$loadBalancerIp""" "docker swarm init --advertise-addr ""$loadBalancerIp"""
 
-echo -e "${Green}Copy nginx.conf to load balancer${NC}"
-scp ".\nginx.conf" root@$loadBalancerIp:default.conf
-
 echo -e "${Green}Join workers to swarm${NC}"
 ssh """root@$workerIp1""" "docker swarm join --token ""$managerToken"""
 # ssh """$workerIp2""" "docker swarm join --token ""$managerToken"""
@@ -28,7 +25,7 @@ ssh """root@$managerIp""" "docker network create --driver overlay minitwit-net"
 
 # create services
 echo -e "${Green}Create services${NC}"
-ssh """root@$managerIp""" "docker service create --name minitwit-souffle --publish 8080:80 --replicas 2 -e ""$CONNECTION_STRING"" -e ""$SERILOG_URI"" ""$DOCKER_USERNAME""/nginx-souffle:latest"
+ssh """root@$managerIp""" "docker service create --name minitwit-souffle --publish 8080:80 --replicas 2 -e ""$CONNECTION_STRING"" -e ""$SERILOG_URI"" ""$DOCKER_USERNAME""/minitwit-souffle:latest"
 
 echo -e "${Green}Create nginx service${NC}"
-ssh """root@$loadBalancerIp""" "docker service create --name nginx-souffle --publish 80:80 --replicas 1 nginx-souffle:latest"
+ssh """root@$loadBalancerIp""" "docker service create --name nginx-souffle --publish 80:80 --replicas 1 ""$DOCKER_USERNAME""/nginx-souffle:latest"
